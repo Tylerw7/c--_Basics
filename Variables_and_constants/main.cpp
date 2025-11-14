cf95064c7898fd3907449898d1b66074b6d9d7f6
Tylerw7 <t4wettach@gmail.com>
2025-11-14 09:15:41 -0500

    update

--- Variables_and_constants/main.cpp
+++ Variables_and_constants/main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 
+
 using namespace std;
 
 // This program will calculate the area of a room in square feet
 
+
+
+void letter_function() {
+    char middle_initial {'g'};
+    cout << "My middle initial is " << middle_initial << endl;
+    
+}
+
 int main() {
 
-    int room_width;
-    int room_length;
+    //int room_width;
+    //int room_length;
+    // letter_function();
 
-    cout << "Enter the width of the room: ";
-    cin >> room_width;
-    cout << "Enter the length of the room: ";
-    cin >> room_length;
-    cout << "The area of the room is: " << room_length * room_width << endl;
 
+    //cout << "Enter the width of the room: ";
+    //cin >> room_width;
+    //cout << "Enter the length of the room: ";
+    //cin >> room_length;
+    //cout << "The area of the room is: " << room_length * room_width << endl;
+
+    unsigned short int exam_score {55};
+    long people_on_earth {7'600'000'000};
+    cout << "My exam score: " << exam_score << "People on earth: " << people_on_earth << endl;
 
 
     return 0;
-}
+};
+
+
+
