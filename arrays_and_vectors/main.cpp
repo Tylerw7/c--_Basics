7fa1e19342e0e0609e4c66a6e280c3c8c885acbd
Tylerw7 <t4wettach@gmail.com>
2025-11-21 15:08:01 -0500

    added multi-dimesnional arrays

--- arrays_and_vectors/main.cpp
+++ arrays_and_vectors/main.cpp
@@ -10,21 +10,40 @@ int main() {
     // cout << "\nThe first vowel is: " << vowels[0] << endl;
     // cout << "\nThe last vowel is: " << vowels[4] << endl;
 
-    int grades [5];
+    // int grades [5];
 
-    cout << "Enter the first 5 grades: ";
-    cin >> grades[0]; 
-    cin >> grades[1];
-    cin >> grades[2];
-    cin >> grades[3];
-    cin >> grades[4];
+    // cout << "Enter the first 5 grades: ";
+    // cin >> grades[0]; 
+    // cin >> grades[1];
+    // cin >> grades[2];
+    // cin >> grades[3];
+    // cin >> grades[4];
 
-    cout << "Grades:" << endl;
-    cout << grades[0] << endl;
-    cout << grades[1] << endl;
-    cout << grades[2] << endl;
-    cout << grades[3] << endl;
-    cout << grades[4] << endl;
+    // cout << "Grades:" << endl;
+    // cout << grades[0] << endl;
+    // cout << grades[1] << endl;
+    // cout << grades[2] << endl;
+    // cout << grades[3] << endl;
+    // cout << grades[4] << endl;
+
+    //multi-dimensional
+
+    const int rows {3};
+    const int cols {4};
+
+    int movie_rating [rows][cols] = {
+        {5,4,3,6},
+        {8,4,5,2},
+        {1,2,7,9},
+    };
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << movie_rating[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
 
 
 
