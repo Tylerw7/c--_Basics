9fad5c4098fa8b435692a677d52b3821be0ac551
Tylerw7 <t4wettach@gmail.com>
2025-11-21 14:57:48 -0500

    update

--- arrays_and_vectors/main.cpp
+++ arrays_and_vectors/main.cpp
@@ -6,10 +6,25 @@ using namespace std;
 
 int main() {
 
-    char vowels [] {'a','e', 'i', 'o', 'u'};
-    cout << "\nThe first vowel is: " << vowels[0] << endl;
-    cout << "\nThe last vowel is: " << vowels[4] << endl;
+    // char vowels [] {'a','e', 'i', 'o', 'u'};
+    // cout << "\nThe first vowel is: " << vowels[0] << endl;
+    // cout << "\nThe last vowel is: " << vowels[4] << endl;
 
+    int grades [5];
+
+    cout << "Enter the first 5 grades: ";
+    cin >> grades[0]; 
+    cin >> grades[1];
+    cin >> grades[2];
+    cin >> grades[3];
+    cin >> grades[4];
+
+    cout << "Grades:" << endl;
+    cout << grades[0] << endl;
+    cout << grades[1] << endl;
+    cout << grades[2] << endl;
+    cout << grades[3] << endl;
+    cout << grades[4] << endl;
 
 
 
