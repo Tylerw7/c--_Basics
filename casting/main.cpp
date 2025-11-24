1196057f9de37559953737cfce542f06fc518860
Tylerw7 <t4wettach@gmail.com>
2025-11-23 19:41:51 -0500

    casting example

--- casting/main.cpp
+++ casting/main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 
 int main() {
 
-    
+    int total {};
+    int num1 {}, num2 {}, num3 {};
+    const int count {3};
+
+    cout << "Enter 3 integers";
+    cin >> num1 >> num2 >> num3;
+
+    total = num1 + num2 + num3;
+    double average = {0.0};
+
+    average = static_cast<double>(total) / count;
+
+    cout << "Total: " << total << "average: " << average;
 
     return 0;
 }
