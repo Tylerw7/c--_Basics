614a7d8699b0e724baaedff1de771786ed67f5cd
Tylerw7 <t4wettach@gmail.com>
2025-12-05 13:38:41 -0500

    if statements

---
+++ if_statements/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    const int first_num {10};
+    const int second_num {5};
+
+    if (first_num > 11) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+}
