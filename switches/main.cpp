778cd8df2f7c1eee2f22a2542e3a60cba54c2eb9
Tylerw7 <t4wettach@gmail.com>
2025-12-05 22:32:39 -0500

    update

--- switches/main.cpp
+++ switches/main.cpp
@@ -9,7 +9,13 @@ int main() {
     cout << "Selected number: " << endl;
     cin >> selection;
 
-    
+    switch (selection) {
+        case '1': cout << '1 selected';
+        case '2': cout << '2 selected';
+        case '3':
+        case '4': cout << "3 or 4 selected";
+        default: cout << '1,2,3,4 NOT selected';
+    }
 
 
 }
